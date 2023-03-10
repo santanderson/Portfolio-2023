3a82af37505b4868198bfc5fd46b21afcb542e1c
Ryan <santoryan1408@gmail.com>
2023-03-10 15:16:40 -0300

    Finalizou estilos

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,9 +1,34 @@
 <script setup>
 const projects = [
     {
-        name: 'BluOcean',
-        img: 'src',
-        link: 'google.com'
+        name: 'Google Interface',
+        img: './src/assets/images/google-interface.png',
+        link: 'https://santanderson.github.io/Google-Interface/',
+        desc: 'Cópia da ui do google.com'
+    },
+    {
+        name: 'Google Interface',
+        img: './src/assets/images/google-interface.png',
+        link: 'https://santanderson.github.io/Google-Interface/',
+        desc: 'Cópia da ui do google.com'
+    },
+    {
+        name: 'Google Interface',
+        img: './src/assets/images/google-interface.png',
+        link: 'https://santanderson.github.io/Google-Interface/',
+        desc: 'Cópia da ui do google.com'
+    },
+    {
+        name: 'Google Interface',
+        img: './src/assets/images/google-interface.png',
+        link: 'https://santanderson.github.io/Google-Interface/',
+        desc: 'Cópia da ui do google.com'
+    },
+    {
+        name: 'Google Interface',
+        img: './src/assets/images/google-interface.png',
+        link: 'https://santanderson.github.io/Google-Interface/',
+        desc: 'Cópia da ui do google.com'
     }
 ]
 </script>
@@ -14,12 +39,17 @@ const projects = [
             <h2>Projetos</h2>
 
             <div class="projectsContainer">
+
                 <div v-for="project in projects">
                     <a :href="project.link" target="_blank">
-                        <h3>{{ project.name }}</h3>
-                        <img src="" :alt="project.name"/>
+                        <div class="info">
+                            <h3>{{ project.name }}</h3>
+                            <span>{{project.desc}}</span>
+                        </div>
+                        <img :src="project.img" :alt="project.name"/>
                     </a>
                 </div>
+
             </div>
         </section>
     </main>
